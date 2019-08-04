04e48db6bd26af9533b69528a76c91ec0987487f
szw <41726196@xs.ustb.edu.cn>
2019-08-04 08:53:36 +0800

    Finished minStack

--- minStack.java
+++ minStack.java
@@ -1,4 +1,13 @@
 import java.util.Stack;
+/**
+ *设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ * push(x) -- 将元素 x 推入栈中
+ * pop() -- 删除栈顶的元素
+ * top() -- 获取栈顶元素
+ * getMin() -- 检索栈中的最小元素
+ */
+//看到题目第一想法是设置一个变量记录最小值，但是实现起来发现一旦栈结构发生改变要重新找最小值，不大好写然后看了解析利用辅助栈然后自己实现了一下
+
 public class minStack {
     private Stack<Integer> minStack;
     private  Stack<Integer> helper;
